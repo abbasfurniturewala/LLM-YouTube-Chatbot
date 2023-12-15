dea104d774ec2dc9dc4fae8e5c15bb5b3aecf452
Abbas Furniturewala <furniturewalaabbas@gmail.com>
2023-12-15 16:51:00 -0500

    Sentiment analysis dynamically

--- frosty_app.py
+++ frosty_app.py
@@ -2,6 +2,18 @@ import openai
 import re
 import streamlit as st
 from prompts import get_system_prompt
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI  
+from langchain.utilities import SQLDatabase 
+from langchain.chains import create_sql_query_chain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+
 
 st.title("📺YOUTUBE BOT")
 
@@ -25,25 +37,81 @@ for message in st.session_state.messages:
         if "results" in message:
             st.dataframe(message["results"])
 
-# If last message is not from assistant, we need to generate a new response
+#If last message is not from assistant, we need to generate a new response
+# if st.session_state.messages[-1]["role"] != "assistant":
+#     with st.chat_message("assistant"):
+#         response = ""
+#         resp_container = st.empty()
+#         for delta in openai.ChatCompletion.create(
+#             model="gpt-4-1106-preview",
+#             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+#             stream=True,
+#         ):
+#             response += delta.choices[0].delta.get("content", "")
+#             resp_container.markdown(response)
+
+#         message = {"role": "assistant", "content": response}
+#         # Parse the response for a SQL query and execute if available
+#         sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+#         if sql_match:
+#             sql = sql_match.group(1)
+#             conn = st.experimental_connection("snowpark")
+#             message["results"] = conn.query(sql)
+#             st.dataframe(message["results"])
+#         st.session_state.messages.append(message)
+
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
-        response = ""
+        # Initialize Langchain chain
+        # prompt = {"role": "system", "content": get_system_prompt()}
+
+        prompt = ChatPromptTemplate(messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a nice chatbot having a conversation with a human."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),]
+)
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        gpt = ChatOpenAI(temperature=0, openai_api_key=st.secrets.OPENAI_API_KEY, model_name= 'gpt-4-1106-preview')
+        llm_chain = LLMChain(llm=gpt, prompt=prompt, verbose=True, memory=memory)  # adjust the model as necessary
+
+        # Process the input using Langchain chain
+        response = llm_chain.run([{"role": m["role"], "content": m["content"]} for m in st.session_state.messages])
+
+        # Display the response
         resp_container = st.empty()
-        for delta in openai.ChatCompletion.create(
-            model="gpt-4-1106-preview",
-            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-            stream=True,
-        ):
-            response += delta.choices[0].delta.get("content", "")
-            resp_container.markdown(response)
+        resp_container.markdown(response)
 
         message = {"role": "assistant", "content": response}
+        # Parse the response for a SQL query and execute if available
+        # ... existing SQL processing logic ...
+
         # Parse the response for a SQL query and execute if available
         sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
         if sql_match:
             sql = sql_match.group(1)
             conn = st.experimental_connection("snowpark")
-            message["results"] = conn.query(sql)
+            sql_results = conn.query(sql)
+            message["results"] = sql_results
             st.dataframe(message["results"])
+
+            query_results_str = str(sql_results)
+
+            sql_results_message = {"role": "assistant", "content": f"SQL Query Results: {query_results_str}"}
+
+            # Append the SQL results message to the conversation history
+            st.session_state.messages.append(sql_results_message)
+
+            # Update the memory with the new message (if your memory implementation supports this)
+            
+        
         st.session_state.messages.append(message)
+        # prepared_messages = []
+        # for m in st.session_state.messages:
+        #     content_str = str(m["content"]) if m["content"] is not None else ""
+        #     prepared_messages.append({"role": m["role"], "content": content_str})
+
+        # response = llm_chain.run(prepared_messages)
+        # #response = llm_chain.run([{"role": m["role"], "content": m["content"]} for m in st.session_state.messages])
--- prompts.py
+++ prompts.py
@@ -1,14 +1,16 @@
 import streamlit as st
+# from transformers import pipeline
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
 
 #QUALIFIED_TABLE_NAME = "FROSTY_SAMPLE.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ANNUAL_TIME_SERIES"
 
-# QUALIFIED_TABLE_NAME = "YOUTUBE_LLM.STAGING.YOUTUBE_LLM_VIDEOS"
+#QUALIFIED_TABLE_NAME = "YOUTUBE_LLM.STAGING.YOUTUBE_LLM_VIDEOS"
 
-# TABLE_DESCRIPTION = """
-# This table has various metrics for Youtube channels (main stream media News channels) for the past month .
-# The user may describe the entities interchangeably as video views, performance, most watched.
-# """
-
+#TABLE_DESCRIPTION = """
+#This table has various metrics for Youtube channels (main stream media News channels) for the past month .
+#The user may describe the entities interchangeably as video views, performance, most watched.
+#"""
 
 # TABLE_DESCRIPTION = """
 # This table has various metrics for financial entities (also referred to as banks) since 1983.
@@ -20,31 +22,32 @@ import streamlit as st
 # If altering, you may also need to modify the formatting logic in get_table_context() below.
 
 #METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM FROSTY_SAMPLE.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ATTRIBUTES_LIMITED;"
-# METADATA_QUERY = ""
+#METADATA_QUERY = ""
 
 GEN_SQL = """
-You will be acting as an AI Snowflake SQL Expert named YOUTUBE BOT.
-Your goal is to give correct, executable sql query to users.
+You will be acting as an AI Snowflake SQL Expert and also as a sentiment analyzer named YOUTUBE BOT.
+Your goal is to give correct, executable sql query and text analytics to users.
 You will be replying to users who will be confused if you don't respond in the character of YOUTUBE BOT.
 You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
-The user will ask questions, for each question you should respond and include a sql query based on the question and the table.
-USE DATABASE "YOUTUBE_LLM" 
-
+The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
+Given the result of a SQL query, provide the following sentiment and text analysis only if the output contains information related to 'COMMENTS':
 {context}
 
-Here are 6 critical rules for the interaction you must abide:
+Here are 8 critical rules for the interaction you must abide:
 <rules>
 1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
 ```sql
 (select 1) union (select 2)
 ```
-2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to the appropriate amount.
 3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
 4. Make sure to generate a single snowflake sql code, not multiple. 
 5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
 6. DO NOT put numerical at the very front of sql variable.
 </rules>
 7. Whenever writing a table name preceed it by databasename eg: YOUTUBE_LLM.table_name
+8. Whenver the user asks a sentiment based question query the youtube_llm_comments table to get the comments for analysis 
+
 
 Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
 and wrap the generated sql code with ``` sql code markdown in this format e.g:
@@ -56,6 +59,50 @@ For each question from the user, make sure to include a query in your response.
 
 Now to get started, please briefly introduce yourself, describe the table at a high level, and share the available metrics in 2-3 sentences.
 Then provide 3 example questions using bullet points.
+
+Given a series of conversational messages or comments from users, analyze and provide the following information:
+1. **Sentiment Analysis:**
+   - Determine the overall sentiment of the messages (positive, negative, or neutral).
+   - Provide a confidence score for the sentiment analysis.
+
+2. **Text Analytics:**
+   - Identify any spam content in the messages.
+   - Highlight any toxic words or phrases present.
+   - Extract key information or entities mentioned in the messages.
+
+3. **Brief Summarization:**
+   - Generate a brief summary of the main topics discussed in the messages.
+   - Keep the summary concise and informative.
+
+4. **Top Trends:**
+   - Identify and list the top trends or recurring themes in the messages.
+   - Provide insights into the most discussed topics.
+
+Ensure that the responses are clear, coherent, and relevant to the content of the input messages. If needed, you can specify additional details or parameters for each analysis. Please provide the results with corresponding confidence scores or any relevant metrics.
+Example Input:
+"I love the new features in your app! It's amazing."
+"Spam: Buy now and get 50 percent off! Limited offer!"
+"The support team was very helpful. Great service."
+"This is the worst product I've ever bought. So disappointed."
+"Toxic: Your company is a scam. I regret using your services."
+
+Analyze the sentiment, identify spam content, toxic words, generate a brief summary, and list the top trends.
+
+Expected Output:
+
+1. Sentiment Analysis:
+Positive sentiment with a confidence score of 0.92
+2. Text Analytics:
+No spam content detected.
+Toxic words: None
+Key information: "new features," "amazing," "support team," "great service"
+3.Brief Summarization:
+Users express positive feedback about the new app features and great service.
+4.Top Trends:
+Trend 1: Positive feedback on new app features.
+Trend 2: Mention of helpful support team.
+Trend 3: Negative sentiment about a disappointing product.
+
 """
 
 # @st.cache_data(show_spinner=False)
@@ -92,24 +139,6 @@ Then provide 3 example questions using bullet points.
 #         )
 #         context = context + f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
 #     return context
-
-
-
-
-
-# def get_system_prompt():
-#     table_context = get_table_context(
-#         table_name=QUALIFIED_TABLE_NAME,
-#         table_description=TABLE_DESCRIPTION,
-#         metadata_query=METADATA_QUERY
-#     )
-#     return GEN_SQL.format(context=table_context)
-
-# # do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
-# if __name__ == "__main__":
-#     st.header("System prompt for YOUTUBE BOT")
-#     st.markdown(get_system_prompt())
-
 
 def get_single_table_context(conn, schema_name, database_name, table_name, table_description):
     columns = conn.query(f"""
@@ -149,12 +178,73 @@ def get_all_tables_context(schema_name: str, database_name: str):
 
     return "\n\n".join(all_contexts)
 
+
 def get_system_prompt():
     all_tables_context = get_all_tables_context(schema_name="STAGING", database_name="YOUTUBE_LLM")
     return GEN_SQL.format(context=all_tables_context)
 
+
+# def distilledbert_test(question, inputdata):
+#     # Initialize the question-answering pipeline
+#     question_answerer = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
+
+#     # Specify your question
+#     #question = "Identify the spam content in the list"
+#     # Use the question-answering pipeline to find the answer
+#     result = question_answerer(question=question, context=inputdata)
+
+#     ans=result['answer']
+#     res=[]
+#     res.append({"Answer":ans})
+#     #print(f"Answer: '{result['answer']}', score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']}") 
+#     return res
+
+# def checkSimilarity(inputques):
+#     questions=["what is the video about based on the comments?", "list the most discussed topics regarding video.", 
+#                "brief about content of the comments","why did the users respond the way they did in this?",
+#                 "what is the sentiment of the video","trending topics in the comments", "what is the spam content in the comments for video",
+#                 "what's most discussed about in this video comments?"]
+#     matched = False
+#     for question in questions:
+#         vectorizer = CountVectorizer().fit_transform([inputques, question])
+#         vectors = vectorizer.toarray()
+
+#         # Calculate cosine similarity
+#         cosine_sim = cosine_similarity(vectors)
+#         if cosine_sim[0][1] >= 0.7:
+#             matched = True
+#             break
+#     return matched
+
+# def get_system_prompt():
+#     table_context = get_table_context(
+#         table_name=QUALIFIED_TABLE_NAME,
+#         table_description=TABLE_DESCRIPTION,
+#         metadata_query=METADATA_QUERY
+#     )
+#     return GEN_SQL.format(context=table_context)
+
+# do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
 if __name__ == "__main__":
     st.header("System prompt for YOUTUBE BOT")
     st.markdown(get_system_prompt())
-
-
+    """
+    user_question = st.text_input("Ask a question:")
+    
+    questions_list=["What is the video about based on the comments?", "List the most discussed topics.", 
+                    "trending topics in the comments", "what is the spam content in the comments",
+                    "What's most discussed about in this?"]
+    if user_question:
+        analysis_ques=checkSimilarity(user_question.lower(), questions_list)
+        # Check if the question is related to sentiment analysis
+        if analysis_ques:
+            # Assume 'inputdata' is the relevant context for DistilledBERT
+            distilledbert_result = distilledbert_test(user_question, inputdata)
+            st.write(f"{user_question} : {distilledbert_result}")
+        else:
+            # Your existing logic to handle other types of questions
+            # ...
+            st.write("Oops, I haven't trained to answer these questions yet")
+        # Generate and display the SQL query based on the user's question
+        # ...
+    """
